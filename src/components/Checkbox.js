3bc9619c3c0164b4db1a691521f2b37fe21ef1c4
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-20 09:24:46 -0700

    add a Checkbox component so users can toggle a todo done or otherwise

---
+++ src/components/Checkbox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Checkbox extends React.Component {
+
+  render() {
+    const { todo, handleStatus } = this.props;
+    
+    return (
+      <div>
+        <input type="checkbox" id={todo.id} checked={todo.completed} 
+        onChange={() => handleStatus(todo.id)} />
+      </div>
+    )
+  }
+ 
+}
+
+export default Checkbox
