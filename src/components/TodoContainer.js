68edf72724f61fa2bf4141a386cf9466e39a4c1c
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-09-20 09:36:42 -0700

    add functions to handle todo addition, status update and deletion

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,14 +1,86 @@
 import React from 'react';
+import Header from './Header';
+import InputTodo from './Input';
+import TodoList from './ListTodos';
 
 // eslint-disable-next-line  react/prefer-stateless-function
-export class TodoContainer extends React.Component {
+class TodoContainer extends React.Component {
+  constructor() {
+    super();
+    this.state = ({
+      todoList: [
+        {
+          id: 1,
+          title: 'Setup dev-env',
+          completed: true,
+        },
+        {
+          id: 2,
+          title: 'Develope website',
+          completed: false,
+        },
+        {
+          id: 3,
+          title: 'Deploy to live server',
+          completed: false,
+        },
+      ],
+    });
+  }
+
+  handleTodoStatus = (id) => {
+    const todoID = id;
+    this.setState((prevState) => ({
+      todoList: prevState.todoList.map((todo) => {
+        if (todo.id === todoID) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        
+        return todo;
+      }),
+    }))
+  };
+
+  handleDeletion = (id) => {
+    const todoID = id;
+    this.setState({
+      todoList: [
+        // eslint-disable-next-line react/destructuring-assignment
+        ...this.state.todoList.filter((todo) => {
+          return todo.id !== todoID;
+        })
+      ]
+    });
+  };
+  
+  addTodo = (todoTitle) => {
+    const newTodo = {
+      id: 4,
+      title: todoTitle,
+      completed: false
+    };
+    
+    this.setState({
+      todoList: [...this.state.todoList, newTodo]
+    });
+  };
+  
   render() {
+    const { todoList } = this.state;
     return (
       <div>
-        <h1>My TO DO APP</h1>
+        <Header />
+        <InputTodo addTodo={this.addTodo} />
+        <TodoList 
+          todoList={todoList} 
+          handleStatus={this.handleTodoStatus} 
+          handleDeletion={this.handleDeletion} />
       </div>
     );
   }
 }
-
+    
 export default TodoContainer;
